518d511ad001c164a0f1b9fecc76c5c51bf00bac
Ana Goncalves <alsgoncalves91@gmail.com>
2020-06-16 16:32:13 +0100

    Rails Task Managed Completed without CSS

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,41 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def create
+    @task = Task.create(tasks_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
